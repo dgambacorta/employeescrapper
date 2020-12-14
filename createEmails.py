772010b01c0fd7d55839011c460748511790e984
dgambacorta <damiangambacorta@gmail.com>
2020-12-14 20:06:28 -0300

    init

--- createEmails.py
+++ createEmails.py
@@ -11,16 +11,23 @@ print("You entered: " + domain)
 typeofemail = input("Type of email: 1- name@domain 2-name.surname@domain 3-firstlettersurname@domain: ")
 print("You entered: " + typeofemail)
 
+googleCalendar = input("Google Calendar: yes/no ")
+print("You entered: " + googleCalendar)
 
 file = open(filename, 'r') 
 lines = file.readlines() 
 output_name = domain.replace('.','')+"_emails.txt"
 output = open(output_name, "w")
 
+if googleCalendar == 'yes' or googleCalendar == 'y':
+        output_name_gcalendar = domain.replace('.','')+"gc.txt"
+        output_gcalendar = open(output_name_gcalendar, "w")
+
 for employee in lines:
     fulldomain="@"+domain
-    employeeFirstName=re.split("[^a-zA-Z]*", employee)[0]
-    employeeLastname=re.split("[^a-zA-Z]*", employee)[1]
+    if(re.split("[^a-zA-Z]*", employee)[0]):
+        employeeFirstName=re.split("[^a-zA-Z]*", employee)[0].lower()
+        employeeLastname=re.split("[^a-zA-Z]*", employee)[1].lower()
 
     if typeofemail == '1':
         email = employeeFirstName+fulldomain
@@ -32,4 +39,12 @@ for employee in lines:
         email = employeeFirstName[0]+employeeLastname+fulldomain
 
     print(email, file=output) 
+
+    if googleCalendar == 'yes' or googleCalendar == 'y':
+        gcalendaremail = "https://calendar.google.com/calendar/u/3/r?cid="+email.lower()
+        print(gcalendaremail, file=output_gcalendar)
+
+
+
+
 
--- employee.py
+++ employee.py
@@ -2,10 +2,13 @@ from linkedin_scraper import Person, actions, Company
 import time
 from selenium import webdriver
 from urllib.parse import urlparse
+import sys
+import re
 
 
-email = 'aaaaaa@gmail.com';  
-password = 'aaaaaaa';
+
+email = 'xxxxx';  
+password = 'xxxxx';
 
 
 target = 'targets.txt'
@@ -28,7 +31,7 @@ for url in Lines:
 
     browser.get(url)
     #browser.maximize_window()
-    SCROLL_PAUSE_TIME = 1
+    SCROLL_PAUSE_TIME = 3
 
     # Get scroll height
     last_height = browser.execute_script("return document.body.scrollHeight")
@@ -53,9 +56,51 @@ for url in Lines:
     for res in results_li:
         test = browser.find_element_by_css_selector(".org-people-profile-card__profile-title.t-black.lt-line-clamp.lt-line-clamp--single-line.ember-view")
         name = (res.text.split("\n") or [""])[0].strip()
+        print (name)
         if name != 'Miembro de LinkedIn' and name != '':
             print(name, file=output) 
-            print(name)
     browser.quit()
 
     output.close()
+
+
+
+filename = output_name
+
+domain = input("Please enter Company domain: eg magento.com: ")
+print("You entered: " + domain)
+
+typeofemail = input("Type of email: 1- name@domain 2-name.surname@domain 3-firstlettersurname@domain: ")
+print("You entered: " + typeofemail)
+
+googleCalendar = input("Google Calendar: yes/no ")
+print("You entered: " + googleCalendar)
+
+file = open(filename, 'r') 
+lines = file.readlines() 
+output_name = domain.replace('.','')+"_emails.txt"
+output = open(output_name, "w")
+
+if googleCalendar == 'yes' or googleCalendar == 'y':
+        output_name_gcalendar = domain.replace('.','')+"gc.txt"
+        output_gcalendar = open(output_name_gcalendar, "w")
+
+for employee in lines:
+    fulldomain="@"+domain
+    employeeFirstName=re.split("[^a-zA-Z]*", employee)[0].lower()
+    employeeLastname=re.split("[^a-zA-Z]*", employee)[1].lower()
+
+    if typeofemail == '1':
+        email = employeeFirstName+fulldomain
+
+    if typeofemail == '2':
+        email = employeeFirstName+'.'+employeeLastname+fulldomain
+
+    if typeofemail == '3':
+        email = employeeFirstName[0]+employeeLastname+fulldomain
+
+    print(email, file=output) 
+
+    if googleCalendar == 'yes' or googleCalendar == 'y':
+        gcalendaremail = "https://calendar.google.com/calendar/u/3/r?cid="+email.lower()
+        print(gcalendaremail, file=output_gcalendar)
